fdd45e68de78aac86d64e16a5fbb01e4322c9907
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-08-11 12:17:40 -0600

    upd: sql script

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c7ccd5a96345b95ded330f2cfb053ff683bf16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab600572861e9aef273df8bbf933c080f1dd49f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- script.sql
+++ script.sql
@@ -1,21 +1,45 @@
-CREATE DATABASE DBMovies;
+USE [master];
+GO
 
-use DBMovies;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DBMovies')
+BEGIN
+    CREATE DATABASE [DBMovies];
+END
+GO
 
-CREATE TABLE Director(
-	PKDirector INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	Name VARCHAR(100),
-	Age INT,
-	Active BIT
-);
+USE [DBMovies];
+GO
 
-CREATE TABLE Movies(
-	PKMovies INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	Name VARCHAR(100),
-	Gender VARCHAR(100),
-	Duration TIME,
-	FKDirector INT REFERENCES Director(PKDirector)
-);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Director')
+BEGIN
+    CREATE TABLE [dbo].[Director](
+        PKDirector INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+        Name VARCHAR(100),
+        Age INT,
+        Active BIT
+    );
+END
+GO
 
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Movies')
+BEGIN
+    CREATE TABLE [dbo].[Movies](
+        PKMovies INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+        Name VARCHAR(100),
+        Gender VARCHAR(100),
+        Duration TIME,
+        FKDirector INT,
+        CONSTRAINT FK_Director FOREIGN KEY (FKDirector) REFERENCES Director(PKDirector)
+    );
+END
+GO
 
+INSERT INTO [dbo].[Director] (Name, Age, Active) VALUES ('Yorgos Lanthimos', 56, 1);
+INSERT INTO [dbo].[Director] (Name, Age, Active) VALUES ('Christopher Nolan', 56, 1);
+INSERT INTO [dbo].[Director] (Name, Age, Active) VALUES ('Quentin Tarantino', 58, 0);
+GO
 
+INSERT INTO [dbo].[Movies] (Name, Gender, Duration, FKDirector) VALUES ('The Lobster', 'Drama', '02:07', 1);
+INSERT INTO [dbo].[Movies] (Name, Gender, Duration, FKDirector) VALUES ('Inception', 'Sci-Fi', '02:28', 2);
+INSERT INTO [dbo].[Movies] (Name, Gender, Duration, FKDirector) VALUES ('Pulp Fiction', 'Crime', '02:34', 3);
+GO
