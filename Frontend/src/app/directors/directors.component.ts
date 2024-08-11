964bdaf07138ce4c360e26643e943976d5c2654c
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-08-10 23:28:00 -0600

    feat: delete director

--- Frontend/src/app/directors/directors.component.ts
+++ Frontend/src/app/directors/directors.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Director } from '../Interfaces/director';
 import { DirectorService } from '../Services/director.service';
+import { ConfirmationService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-directors',
@@ -11,7 +13,7 @@ export class DirectorsComponent implements OnInit {
 
   directors : Director[] = [];
 
-  constructor(private directorService: DirectorService) { }
+  constructor(private router: Router,private directorService: DirectorService, private confirmationService: ConfirmationService) { }
 
   ngOnInit(): void {
     this.loadDirectors();
@@ -26,6 +28,21 @@ export class DirectorsComponent implements OnInit {
         console.error("Error loading directors", error);
       }
     })
+  }
+
+
+  deleteMovie(idDirector: number) {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to delete this movie?',
+      accept: () => {
+        this.directorService.delete(idDirector).subscribe({
+          next: () => this.loadDirectors(),
+          error: (error) => {
+            console.error('Error deleting movie', error);
+          }
+        });
+      }
+    });
   }
 
 }
