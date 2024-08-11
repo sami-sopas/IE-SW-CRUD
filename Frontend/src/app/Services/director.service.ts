bfa6ffe0b7197bc0d3fbc1bafd9c633125839b11
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-08-10 22:40:31 -0600

    feat: director service

--- Frontend/src/app/Services/director.service.ts
+++ Frontend/src/app/Services/director.service.ts
@@ -11,12 +11,27 @@ import { Director } from '../Interfaces/director';
 export class DirectorService {
 
   private endpoint : string = environment.endPoint;
-
-  private apiUrl : string = this.endpoint + "directors";
+  private apiUrl : string = this.endpoint + "director/";
 
   constructor(private http:HttpClient) { }
 
   getList():Observable<Director[]>{
-    return this.http.get<Director[]>(this.apiUrl);
+    return this.http.get<Director[]>("directors/");
+  }
+
+  add(director: Director): Observable<Director> {
+    return this.http.post<Director>(`${this.apiUrl}create`, director);
+  }
+
+  get(idDirector: number): Observable<Director> {
+    return this.http.get<Director>(`${this.apiUrl}/${idDirector}`);
+  }
+
+  update(idDirector: number, director: Director): Observable<Director> {
+    return this.http.put<Director>(`${this.apiUrl}update/${idDirector}`, director);
+  }
+
+  delete(idDirector: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}delete/${idDirector}`);
   }
 }
